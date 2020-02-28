0aa299e2b437f4a584ab245fe633e3670387f754
ArzArm <armine.arzumanyan@gmail.com>
2020-02-28 12:29:20 +0400

    NightMare1

--- NightMare/src/Problem1.java
+++ NightMare/src/Problem1.java
@@ -1,21 +1,21 @@
 public class Problem1 {
     public static void main(String[] args) {
-        int[] arr = new int[]{0, -5, -2, -3, -2, 0, 2, -7};
-        int min = 0;
+        int[] arr = new int[]{2, 3, 4, 5, 6, -7, 8, 9, 1, 0};
+        int min = 1;
         boolean isValueExist = true;
 
         while (isValueExist) {
             isValueExist = false;
-            min++;
             for (int i = 0; i < arr.length; i++) {
                 if (arr[i] == min) {
                     isValueExist = true;
+                    min++;
                     break;
                 }
             }
             if (!isValueExist) {
                 System.out.println(min);
-                break;
+
             }
         }
     }
