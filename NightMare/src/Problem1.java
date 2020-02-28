57d941d4df77ee8f0a675f1509af7e51b0dbfd0f
ArzArm <armine.arzumanyan@gmail.com>
2020-02-28 19:44:27 +0400

    NightMare1

--- NightMare/src/Problem1.java
+++ NightMare/src/Problem1.java
@@ -1,23 +1,31 @@
 public class Problem1 {
     public static void main(String[] args) {
-        int[] arr = new int[]{2, 3, 4, 5, 6, -7, 8, 9, 1, 0};
-        int min = 1;
-        boolean isValueExist = true;
+        int[] arr = new int[]{0, 110, 1, -2, 0, -2, 0, 3, 10, 1256, 0, 2};
 
-        while (isValueExist) {
-            isValueExist = false;
-            for (int i = 0; i < arr.length; i++) {
-                if (arr[i] == min) {
-                    isValueExist = true;
-                    min++;
-                    break;
-                }
-            }
-            if (!isValueExist) {
-                System.out.println(min);
-
+        int i, j;
+        int max = 1;
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
             }
         }
+        int[] newArr = new int[max];
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                newArr[arr[i] - 1] = arr[i];
+            }
+        }
+        for (j = 0; j < newArr.length; j++) {
+            if (newArr[j] == 0) {
+                int smallestNumber = j + 1;
+                System.out.println("The smallest positive integer which does not occur in array is: " + smallestNumber);
+                break;
+            }
+        }
+
+
     }
+
 }
+
 
