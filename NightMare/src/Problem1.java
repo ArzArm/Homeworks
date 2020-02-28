6a9baf9143a78a17fa7ec5ed67c4e0a2ebc4bf82
ArzArm <armine.arzumanyan@gmail.com>
2020-02-28 22:05:08 +0400

    NightMare1

--- NightMare/src/Problem1.java
+++ NightMare/src/Problem1.java
@@ -1,15 +1,13 @@
 public class Problem1 {
     public static void main(String[] args) {
-        int[] arr = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, 1002, 13, 1154, -15, 0, 17, 21, 18, 19, 20, 22, 24};
+        int[] arr = new int[]{25986, 25698, -896325, 1259870, 2222, 1, 2, 0};
         int i, j;
 
-        int[] newArr = new int[arr.length * 2];
+        int[] newArr = new int[arr.length];
         for (i = 0; i < arr.length; i++) {
             if (arr[i] > 0) {
                 if (arr[i] <= arr.length) {
                     newArr[arr[i] - 1] = arr[i];
-                } else {
-                    newArr[arr.length + i] = arr[i];
                 }
             }
         }
