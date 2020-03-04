b52885833617422b4b57c15014b1f5c50291c6f3
ArzArm <armine.arzumanyan@gmail.com>
2020-03-04 10:23:00 +0400

    Homework6

--- HomeWork6/src/Problem6.java
+++ HomeWork6/src/Problem6.java
@@ -4,7 +4,7 @@ public class Problem6 {
     public static void main(String[] args) {
         System.out.println("Input parenthesis sequence : ");
         Scanner scanner = new Scanner(System.in);
-        String sequence = scanner.next();
+        String sequence = scanner.nextLine();
         int counter = 0;
         boolean isLoopCompleted = true;
 
@@ -14,25 +14,28 @@ public class Problem6 {
             for (int i = 0; i < sequence.length(); i++) {
                 if (sequence.charAt(i) == '(') {
                     counter++;
-                } else {
+                } else if (sequence.charAt(i) == ')') {
                     counter--;
+                } else {
+                    System.out.println("Sequence contains untrue symbol!!!!");
+                    isLoopCompleted = false;
+                    break;
                 }
-                if (counter == 0 && i != sequence.length()-1) {
+                if (counter == 0 && i != sequence.length() - 1) {
                     if (sequence.charAt(i + 1) == ')') {
                         System.out.println("Invalid");
-                        isLoopCompleted  = false;
+                        isLoopCompleted = false;
                         break;
                     }
                 }
             }
-            while (isLoopCompleted ) {
+            if (isLoopCompleted) {
                 if (counter != 0) {
                     System.out.println("Invalid");
                 } else {
                     System.out.println("Valid");
-                    break;
                 }
-        }
+            }
         }
     }
 }
